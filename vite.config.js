8215dcbaebc6536e549d8f04ceffc7fb44c12788
Emmanuel Giraldo <esemmanuel2022@gmail.com>
2023-06-02 16:06:42 -0500

    ruta de vite gihub

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:" https://esgg1.github.io/React-Vite-gh-pages"
 })
